a217f482785137a722cf0990f583ce3eca821594
Aditya Bisht <adityabisht64@gmail.com>
2023-02-07 16:18:30 +0530

    Passing lint

--- packages/site/src/features/confirm-snap/Confirm.tsx
+++ packages/site/src/features/confirm-snap/Confirm.tsx
@@ -15,34 +15,12 @@ const CONFIRM_SNAP_ID = 'npm:@metamask/test-snap-confirm';
 const CONFIRM_SNAP_PORT = 8004;
 
 export const Confirm: FunctionComponent = () => {
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [textAreaContent, setTextAreaContent] = useState('');
   const [invokeSnap, { isLoading, data }] = useInvokeMutation();
-
-  const provider = new ethers.providers.Web3Provider(window.ethereum as any);
-  // await provider.send('eth_requestAccounts', []);
-  const owner: ethers.providers.JsonRpcSigner = provider.getSigner();
-
-  // const handleChange =
-  //   (fn: Dispatch<SetStateAction<string>>) =>
-  //   (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-  //     fn(event.target.value);
-  //   };
-
-  const handleSubmit = () => {
-    invokeSnap({
-      snapId: getSnapId(CONFIRM_SNAP_ID, CONFIRM_SNAP_PORT),
-      method: 'confirm',
-      params: [owner as any, description, textAreaContent],
-    });
-  };
 
   const handleCreate = () => {
     invokeSnap({
       snapId: getSnapId(CONFIRM_SNAP_ID, CONFIRM_SNAP_PORT),
       method: 'create',
-      // params: [owner as ethers.providers.JsonRpcSigner],
     });
   };
 
